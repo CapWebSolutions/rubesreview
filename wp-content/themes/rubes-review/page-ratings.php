22ac2585c2594c1756413c12c25282a76b1ee872
Matt Ryan <matt@capwebsolutions.com>
2018-01-08 17:00:12 -0500

    Clean up code

--- wp-content/themes/rubes-review/page-ratings.php
+++ wp-content/themes/rubes-review/page-ratings.php
@@ -20,18 +20,8 @@ function rubesreview_add_body_class( $classes ) {
 	return $classes;
 }
 
-// Remove Skip Links.
-// remove_action ( 'genesis_before_header', 'genesis_skip_links', 5 );
-
-// Dequeue Skip Links Script.
-// add_action( 'wp_enqueue_scripts', 'rubesreview_dequeue_skip_links' );
-function rubesreview_dequeue_skip_links() {
-	wp_dequeue_script( 'skip-links' );
-}
-
 // Force Content/Sidebar layout.
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_content_sidebar' );
-// add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 
 /**
  * Remove the standard loop
