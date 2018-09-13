86dc43e09e6dd4c1bc20166a33f32eed6bc7f81a
Matt Ryan <matt@capwebsolutions.com>
2018-09-13 13:03:31 -0400

    Update footer with registertrade mark info

--- wp-content/plugins/rubesreview-core-functionality/lib/functions/core-footer.php
+++ wp-content/plugins/rubesreview-core-functionality/lib/functions/core-footer.php
@@ -33,7 +33,7 @@ function cap_web_footer_creds_filter( $creds ) {
 	
 	$creds = '<div class="footer-alignleft">';
 	$creds .= '<a href="/privacy-policy/">Privacy Policy</a> &middot; <a href="/terms-conditions/">Terms & Conditions</a><br/>';
-	$creds .= 'Copyright [footer_copyright] [sitename] &middot; All Rights Reserved.';
+	$creds .= 'Copyright [footer_copyright] [sitename] &middot; All Rights Reserved.<br>Rube\'s Review trademarks are federally registered trademark owned by Rube\'s Review Company. Any unauthorized use is expressly prohibited.';
 	$creds .= '</div><div class="footer-alignright">';
 	$creds .= "Website by <a {$rel} href=\"https://capwebsolutions.com/\" target=\"_blank\" >Cap Web Solutions</a><br>";
 	$creds .= '[footer_custombacktotop]';
